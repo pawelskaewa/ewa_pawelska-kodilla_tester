006ef9ab22efe6214c92b91ae7204e7b4538b604
EWA PAWELSKA <pawelska.ewa@gmail.com>
2022-01-28 12:51:28 +0100

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");
+        System.out.println("Ewa");
+        System.out.println("30");
+        System.out.println("P");// [3]
+    }
+}
