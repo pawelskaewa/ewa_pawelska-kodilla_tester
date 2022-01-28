e9365e10f092ec796d91f971c70c195e27735c90
EWA PAWELSKA <pawelska.ewa@gmail.com>
2022-01-28 13:44:55 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Ewa";
+        int yearOfBirth = 1991;
+        int currentYear = 2022;
+        double height = 1.63;
+        char myFavoriteLetter = 'E';
+        boolean isGoodStudent = true;
+        System.out.println(myName);
+        System.out.println(yearOfBirth);
+        System.out.println(currentYear);
+        System.out.println(height);
+        System.out.println(myFavoriteLetter);
+        System.out.println(isGoodStudent);
+    }
+}
