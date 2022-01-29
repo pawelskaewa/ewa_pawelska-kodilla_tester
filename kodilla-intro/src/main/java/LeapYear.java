b04f05d2c27daaf489599c393b3e59e3bfd7aa44
EWA PAWELSKA <pawelska.ewa@gmail.com>
2022-01-29 17:40:02 +0100

    LeapYear

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,29 @@
+public class LeapYear {
+
+    public static void main(String[] args) {
+        int yearInQuestion = 2000;
+        int reminderOfDivision4 = yearInQuestion%4;
+        int reminderOfDivision100 = yearInQuestion%100;
+        int reminderOfDivision400 = yearInQuestion%400;
+        boolean result;
+
+        System.out.println(yearInQuestion);
+        System.out.println(reminderOfDivision4);
+        System.out.println(reminderOfDivision100);
+        System.out.println(reminderOfDivision400);
+
+
+        if (reminderOfDivision4 != 0){
+            result = false;
+        } else if (reminderOfDivision4 == 0 && reminderOfDivision100 != 0) {
+            result = true;
+        } else if (reminderOfDivision4 == 0 && reminderOfDivision100 == 0 && reminderOfDivision400 ==0) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        System.out.println(result);
+
+    }
+}
