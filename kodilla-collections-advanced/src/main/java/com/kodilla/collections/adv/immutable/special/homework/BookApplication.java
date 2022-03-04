080d6ec079706280966bd039cbce7bfb14bca4c9
EWA PAWELSKA <pawelska.ewa@gmail.com>
2022-03-04 10:43:49 +0100

    wywalone TaskHacked;)

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -0,0 +1,23 @@
+package com.kodilla.collections.adv.immutable.special.homework;
+
+import static com.kodilla.collections.adv.immutable.special.homework.BookManager.books;
+
+public class BookApplication {
+    public static void main(String[] args) {
+        Book dziady = BookManager.createBook("Dziady", "Adam Mickiewicz");
+        Book potop = BookManager.createBook("Potop", "Henryk Sienkiewicz");
+        Book ludzieBezdomni = BookManager.createBook("Ludzie bezdomni", "Stefan Zeromski");
+        Book ludzieBezdomni2 = BookManager.createBook("Ludzie bezdomni", "Stefan Zeromski");
+
+        books.add(dziady);
+        books.add(potop);
+        books.add(ludzieBezdomni);
+        books.add(ludzieBezdomni2);
+
+        System.out.println(ludzieBezdomni == ludzieBezdomni2);
+        System.out.println(dziady == ludzieBezdomni);
+        System.out.println(ludzieBezdomni.equals(ludzieBezdomni2));
+
+        System.out.println(dziady.getAuthor());
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -0,0 +1,16 @@
+package com.kodilla.collections.adv.immutable.special.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookManager {
+
+    static List<Book> books = new ArrayList<>();
+
+    public static Book createBook(String title, String author) {
+        for (Book book : books) {
+            if ()
+        }
+        return new Book(title, author);
+    }
+}
