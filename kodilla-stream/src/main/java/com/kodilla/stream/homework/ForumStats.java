36765aca5e8797f00be431083dd71e9c7017a8dc
EWA PAWELSKA <pawelska.ewa@gmail.com>
2022-03-16 13:48:34 +0100

    Forum stats - zadanie 6.3

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.UsersRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        Double averagePostsForUsersAboveForty = UsersRepository.getUsersList()
+                .stream()
+                .filter(u -> u.getAge() >= 40)
+                .mapToInt(n -> n.getNumberOfPost())
+                .average()
+                .getAsDouble();
+        System.out.println("The average of posts for users older than 40: " + averagePostsForUsersAboveForty);
+
+        Double averagePostsForUsersBelowForty = UsersRepository.getUsersList()
+                .stream()
+                .filter(u -> u.getAge() < 40)
+                .mapToInt(n -> n.getNumberOfPost())
+                .average()                                 // [3]
+                .getAsDouble();
+        System.out.println("The average of posts for users younger than 40: " + averagePostsForUsersBelowForty);
+    }
+}
