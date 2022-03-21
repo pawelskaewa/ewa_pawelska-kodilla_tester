5247a25faa9743a2d7db6e44491d33888c5fa012
EWA PAWELSKA <pawelska.ewa@gmail.com>
2022-03-21 12:48:08 +0100

    zad 7.1

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTestSuite.java
@@ -0,0 +1,40 @@
+package com.kodilla.parametrized_tests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringValidatorTestSuite {
+
+    private StringValidator validator = new StringValidator();
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldReturnTrueIfStringIsEmpty(String text) {
+        System.out.println("|" + text + "|");
+        assertTrue(validator.isBlank(text));
+    }
+
+    @Test
+    public void shouldReturnFalseIfStringIsNotEmpty() {
+        assertFalse(validator.isBlank("test"));
+    }
+
+    @Test
+    public void shouldReturnTrueIfStringIsEmpty() {
+        assertTrue(validator.isBlank(""));
+    }
+
+    @Test
+    public void shouldReturnTrueIfStringHasOnlySpaces() {
+        assertTrue(validator.isBlank("   "));
+    }
+
+    @Test
+    public void shouldReturnTrueIfStringIsNull() {
+        assertTrue(validator.isBlank(null));
+    }
+}
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -0,0 +1,40 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserValidatorTestSuite {
+
+    UserValidator userValidator = new UserValidator();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"password", "098765", "bTYUjbhgt75", "09877.", "se4567ujkkkkkkkk.", "._-", "667"})
+    public void testPasswordWithCorrectCharacters(String input) {
+        boolean result = userValidator.validateUsername(input);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"hjk[", "098765&*(", "bhTYUhgt75#$%", "!@#09877.", "/se4567ujkkkkkkkk.", "._-_)", "6"})
+    public void testPasswordWithIncorrectCharacters(String input) {
+        boolean result = userValidator.validateUsername(input);
+        assertFalse(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"email@email.com", "email.email@email.com", "email123@email123.com", "EMAIL@EMAIL.COM", "EMAIL.EMAIL123-_@EMAILL.EMAIL.COM", "ERTwer.ertyFGH@RTYU678567.RTY", "E.E@E.E"})
+    public void testEmailWithCorrectCharacters(String input) {
+        boolean result = userValidator.validateEmail(input);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"hjk[", "098765&*(", "bhTYUhgt75#$%", "email123-_;'.EMAIL.EMAIL@email.com", "EMAIL\\EMAIL123-_@EMAILL.EMAIL.COM", "email123@", "@", "E@E", ".EMAIL@EMAIL.COM"})
+    public void testEmailWithIncorrectCharacters(String input) {
+        boolean result = userValidator.validateEmail(input);
+        assertFalse(result);
+    }
+}
