dffa872cfff66ba14eb1b106b7ad5709a0268a7c
EWA PAWELSKA <pawelska.ewa@gmail.com>
2022-03-30 14:33:36 +0200

    zadanie 7.4 zamowienia

--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
@@ -1,28 +1,44 @@
 package com.kodilla.execution_model.homework;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Shop {
 
     public ArrayList<Order> listOfOrders = new ArrayList<Order>();
 
     public void addNewOrder(Order order) {
-
+        if (order.getOrderValue() > 0)
+            listOfOrders.add(order);
     }
 
-    public ArrayList getListOfOrdersNoOlderThanTwoYears() {
-        return new ArrayList<>();
+    public List getListOfOrdersNoOlderThanTwoYears() {
+        List<Order> ordersNoOlderThanTwoYears = new ArrayList<>();
+        for (int n = 0; n < listOfOrders.size(); n++)
+            if (listOfOrders.get(n).getOrderDate().isAfter(LocalDate.now().minusYears(2))) {
+                ordersNoOlderThanTwoYears.add(listOfOrders.get(n));
+            }
+        return ordersNoOlderThanTwoYears;
     }
 
-    public ArrayList getListOfOrders_Range(double minOrderValue, double maxOrderValue) {
-        return new ArrayList<>();
+    public List getListOfOrdersInRange(double minOrderValue, double maxOrderValue) {
+        List<Order> ordersInRange = new ArrayList<>();
+        for (int n = 0; n < listOfOrders.size(); n++)
+            if (listOfOrders.get(n).getOrderValue() >= minOrderValue & listOfOrders.get(n).getOrderValue() <= maxOrderValue) {
+                ordersInRange.add(listOfOrders.get(n));
+            }
+        return ordersInRange;
     }
 
     public int getNumberOfOrders() {
         return listOfOrders.size();
     }
 
-    public double getOrdersValue(double ordersValue) {
-        return ordersValue;
+    public double getOrdersValue() {
+        double sumOfOrdersValues = 0;
+        for (int n = 0; n < listOfOrders.size(); n++)
+            sumOfOrdersValues += listOfOrders.get(n).getOrderValue();
+        return sumOfOrdersValues;
     }
 }
